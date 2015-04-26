20af0c9a1e41d0c5f02f2d657b36b44853e2ef5d
booox <booox@users.noreply.github.com>
2015-04-26 21:21:47 +0800

    getproxy python
    getproxy python

---
+++ getproxy/getproxy.py
@@ -0,0 +1,33 @@
+﻿#coding=utf-8
+
+import requests
+import os
+import re
+
+
+# get pages num
+def get_pages_num(url):
+    """
+        return a integer for pages num
+    """
+    pass
+    
+# get proxies from a page_url
+def get_proxies(page_url):
+    
+    pass
+
+
+def main():
+    
+    url = 'http://www.proxy.com.ru/'
+    
+    pages_num = get_pages_num(url)
+    
+    for i in range(1, pages_num + 1):
+        
+        page_url = url + '/list_' + str(i) + '.html'
+        proxies = get_proxies(page_url)     #  a list
+        
+        # write the proxies into a file
+        
